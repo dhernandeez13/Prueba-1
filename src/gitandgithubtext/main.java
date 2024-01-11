416db2ce878fe777d0607e1d566e66c0cb346369
dani <dani@DESKTOP-3H8R7OR>
2024-01-11 15:55:34 +0100

    Inicializacion de mi nueva aplicacion

---
+++ src/gitandgithubtext/main.java
@@ -0,0 +1,13 @@
+
+package gitandgithubtext;
+
+
+public class main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Esta linea de codigo viene de local");
+ 
+    }
+    
+}
